bfedf8550369a9c00f7d30145d0e7fb86f162dbc
brknkl <berkanankal68@gmail.com>
2021-11-18 15:00:35 +0300

    added loading spinner

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -11,9 +11,11 @@ import {
 } from "reactstrap";
 import axios from "axios";
 import { useParams } from "react-router-dom";
+import Loader from "react-loader-spinner";
 
 const MovieDetails = () => {
   const [movie, setMovie] = useState({});
+  const [loading, setLoading] = useState(false);
 
   const { id } = useParams();
 
@@ -21,6 +23,10 @@ const MovieDetails = () => {
     axios
       .get(`http://localhost:4000/movies/${id}`)
       .then((res) => setMovie(res.data));
+
+    setTimeout(() => {
+      setLoading(true);
+    }, 1000);
   };
 
   useEffect(() => {
@@ -28,21 +34,32 @@ const MovieDetails = () => {
   }, [id]);
 
   return (
-    <Card>
-      <Row>
-        <Col md="3">
-          <CardImg alt="Card image cap" src={movie.imageUrl} top width="100%" />
-        </Col>
-        <Col md="9">
-          <CardBody>
-            <CardTitle tag="h5">{movie.name}</CardTitle>
-            <CardText>{movie.overview}</CardText>
-            <h4>
-              <Badge color="dark">{movie.rating}</Badge>
-            </h4>
-          </CardBody>
-        </Col>
-      </Row>
+    <Card style={{ height: "400px" }}>
+      {loading ? (
+        <Row>
+          <Col md="3">
+            <CardImg
+              alt="Card image cap"
+              src={movie.imageUrl}
+              top
+              width="100%"
+            />
+          </Col>
+          <Col md="9">
+            <CardBody>
+              <CardTitle tag="h5">{movie.name}</CardTitle>
+              <CardText>{movie.overview}</CardText>
+              <h4>
+                <Badge color="dark">{movie.rating}</Badge>
+              </h4>
+            </CardBody>
+          </Col>
+        </Row>
+      ) : (
+        <div className="d-flex justify-content-center my-auto">
+          <Loader type="Oval" color="#00BFFF" height={100} width={100} />
+        </div>
+      )}
     </Card>
   );
 };
